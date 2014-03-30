a6e23ac62d19174c6a2a04d4827b88c1558c2ae8
Youssef S <youssef.a.selim@gmail.com>
2014-03-30 14:54:34 -0400

    Added project files, database table create @ models

---
+++ Inventories/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils import timezone
+from datetime import datetime
+
+class Inventory(models.Model):
+	#Model Attributes
+	name = models.CharField(max_length=200)
+	description = models.CharField(max_length=600)
+	date_created = models.DateTimeField('Date Created', default=timezone.now())
+
+	def __unicode__(self):
+		return self.name + ': ' + self.description
+
+
+class Item(models.Model):
+	#Relationships with Inventory
+	inventory = models.ForeignKey(Inventory)
+	#Model Attributes
+	item_number = models.IntegerField(blank=False)
+	description = models.CharField(max_length=600)
+	quantity = models.CharField(max_length=100)
+	amount = models.CharField(max_length=100)
+	cases = models.CharField(max_length=100)
+	def_price = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+	curr_price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+	last_modified = models.DateTimeField('Last Modified', default=timezone.now())
+
+	def __unicode__(self):
+		return 'Item#' + self.item_number + ': '+ self.description
+
+
+class Transaction(models.Model):
+	#RelationShips with Inventory & Item
+	inventory = models.ForeignKey(Inventory)
+	items = models.ManyToManyField(Item)
+	#Model Attributes
+	description = models.CharField(max_length=600)
+	date_created = models.DateTimeField('Date Created', default=timezone.now())
+	last_modified = models.DateTimeField('Last Modified', default=timezone.now())
+	fees = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+	#Transaction Type
+	TRANSACTION_TYPE = (('RE', 'Reciept'), ('TI', 'Ticket'),)
+	t_type = models.CharField(max_length=2, choices=TRANSACTION_TYPE)
+
+	def __unicode__(self):
+		return 'Transaction#' + self.id + ': ' + self.description
---
+++ StoreManagement/settings.py
@@ -0,0 +1,85 @@
+"""
+Django settings for StoreManagement project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Templates Directory
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'sfs*yp&x9c-tqb&$54%umx4k49lw554w(@2dnqhbm26x$q20^!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'Inventories'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'StoreManagement.urls'
+
+WSGI_APPLICATION = 'StoreManagement.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/New_York'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
